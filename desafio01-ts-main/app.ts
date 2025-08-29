069df08b353e28a1082eeda301ceff9a88817b23
Augusto Honorata <contatoaugustohonorata@gmail.com>
2025-08-29 17:56:33 -0300

    feat: initialize first challenge project

---
+++ desafio01-ts-main/app.ts
@@ -0,0 +1,9 @@
+import { CompanyAccount } from "./class/CompanyAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
+
+const peopleAccount: PeopleAccount = new PeopleAccount(1, "Nath", 10);
+console.log(peopleAccount);
+peopleAccount.deposit(12);
+const companyAccount: CompanyAccount = new CompanyAccount("DIO", 20);
+companyAccount.deposit(20);
+console.log(companyAccount);
